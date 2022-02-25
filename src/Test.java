d935b25fee98b1b5092b29f3fbd9fcb3b0eb2134
neko <2292042@qq.com>
2022-02-25 22:11:39 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.FutureTask;
+
+/**
+ * 2 * @Author: Crimson
+ * 3 * @Date: 2022/2/25 22:07
+ * 4
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
